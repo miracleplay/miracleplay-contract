b952e6345213f8b3e61ff2cf6b952e237f991040
Artwide <rkafkko@gmail.com>
2023-04-01 20:41:11 +0900

    edit

--- contract/Miracle-Staking/IMiracle-Stake-R3.sol
+++ contract/Miracle-Staking/IMiracle-Stake-R3.sol
@@ -17,7 +17,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 import "./module/Miralce-Stake-Core.sol";
 
-contract StakeMiracleR2 is StakeMiracleCore
+contract StakeMiracleR3 is StakeMiracleCore
 {
     constructor(address _defaultAdmin, uint256 _stakingsection, DropERC1155 _NodeNFTToken, TokenERC20 _RewardToken, address _DaoAddress, uint256 _rewardPerMin, uint256 _alreadyClaimed) {
         StakingSection = _stakingsection;
