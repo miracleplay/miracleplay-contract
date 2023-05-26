6497f0dca311c44cda6e97cb55d425ec15bf5481
ArtWide <rkafkko@gmail.com>
2023-05-26 16:31:48 +0900

    edit

--- contract/Miracle-Tournament/Miracle-Tournament-Score-G1.sol
+++ contract/Miracle-Tournament/Miracle-Tournament-Score-G1.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.17;
 
 import "./Miracle-Escrow-G1.sol";
 import "./Miracle-ProxyV2.sol";
+import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
+import "@thirdweb-dev/contracts/extension/Multicall.sol";
 
 //    _______ _______ ___ ___ _______ ______  ___     ___ ______  _______     ___     _______ _______  _______ 
 //   |   _   |   _   |   Y   |   _   |   _  \|   |   |   |   _  \|   _   |   |   |   |   _   |   _   \|   _   |
@@ -14,7 +16,7 @@ import "./Miracle-ProxyV2.sol";
 //   `-------`-------' `---' `-------`--- ---`-------`---`--- ---`-------'   `-------`--- ---`-------'`-------'
 //   ScoreTournament V0.1.2
 
-contract ScoreTournament is ProxyStorage {
+contract ScoreTournament is ProxyStorage, PermissionsEnumerable, Multicall {
 
     address payable public EscrowAddr;
     uint[] private OnGoingTournaments;
@@ -53,30 +55,17 @@ contract ScoreTournament is ProxyStorage {
     event TournamentEnded(uint tournamentId);
     event TournamentCanceled(uint tournamentId);
 
+    bytes32 public constant UPDATE_ROLE = keccak256("UPDATE_ROLE");
+    bytes32 public constant ESCROW_ROLE = keccak256("ESCROW_ROLE");
+
     constructor(address adminAddress) {
-        admin = adminAddress;
-    }
-
-    modifier onlyAdmin(){
-        require(msg.sender == admin, "Only admin can call this function");
-        _;
-    }
-
-    modifier onlyEscrow(){
-        require(msg.sender == EscrowAddr,  "Only escorw contract can call this function");
-        _;
+        _setupRole(DEFAULT_ADMIN_ROLE, adminAddress);
     }
 
     modifier registrationOpen(uint tournamentId) {
         Tournament storage tournament = tournamentMapping[tournamentId];
         require(block.timestamp >= tournament.registerStartTime, "Registration has not started yet");
         require(block.timestamp <= tournament.registerEndTime, "Registration deadline passed");
-        _;
-    }
-
-    modifier onlyOrganizer(uint tournamentId) {
-        Tournament storage tournament = tournamentMapping[tournamentId];
-        require(msg.sender == tournament.organizer, "Only organizer can call this function");
         _;
     }
 
@@ -93,11 +82,17 @@ contract ScoreTournament is ProxyStorage {
         _;
     }
 
-    function connectEscrow(address payable _escrowAddr) public onlyAdmin {
+    function setUpdateRole(address _updateAddr) external onlyRole(DEFAULT_ADMIN_ROLE){
+        _setupRole(UPDATE_ROLE, _updateAddr);
+    }
+
+    function connectEscrow(address payable _escrowAddr) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _setupRole(ESCROW_ROLE, _escrowAddr);
         EscrowAddr = _escrowAddr;
     }
 
-    function createTournament(uint _tournamentId, uint _registerStartTime, uint _registerEndTime, uint _tournamentStartTime, uint _tournamentEndTime, uint _prizeCount, string memory _tournamentURI) public onlyEscrow {
+
+    function createTournament(uint _tournamentId, uint _registerStartTime, uint _registerEndTime, uint _tournamentStartTime, uint _tournamentEndTime, uint _prizeCount, string memory _tournamentURI) public onlyRole(ESCROW_ROLE) {
         Tournament storage newTournament = tournamentMapping[_tournamentId];
         newTournament.registered = true;
         newTournament.organizer = payable(msg.sender);
@@ -113,7 +108,7 @@ contract ScoreTournament is ProxyStorage {
         emit CreateTournament(_tournamentId);
     }
 
-    function register(uint tournamentId, address _player) public payable registrationOpen(tournamentId) {
+    function register(uint tournamentId, address _player) public payable registrationOpen(tournamentId) onlyRole(ESCROW_ROLE){
         Tournament storage tournament = tournamentMapping[tournamentId];
         require(block.timestamp >= tournament.registerStartTime, "Registration has not started yet");
         require(block.timestamp <= tournament.registerEndTime, "Registration deadline passed");
@@ -133,7 +128,7 @@ contract ScoreTournament is ProxyStorage {
     }
 
 
-    function updateScore(uint tournamentId, address _account, uint _score) public onlyAdmin tournamentNotStarted(tournamentId) tournamentEndedOrNotStarted(tournamentId) {
+    function _updateScore(uint tournamentId, address _account, uint _score) internal {
         Tournament storage tournament = tournamentMapping[tournamentId];
         require(tournament.players[tournament.playerIdByAccount[_account]].isRegistered, "Player is not registered");
 
@@ -143,8 +138,14 @@ contract ScoreTournament is ProxyStorage {
         emit ScoreUpdated(tournamentId, _account, _player.score);
     }
 
+    function updateScore(uint tournamentId, address _account, uint[] calldata _score) external onlyRole(UPDATE_ROLE) {
+        for(uint i = 0; i < _score.length; i++) {
+            _updateScore(tournamentId, _account, _score[i]);
+        }
+    }
 
-    function calculateRanking(uint tournamentId) public onlyAdmin {
+
+    function calculateRanking(uint tournamentId) internal {
         Tournament storage tournament = tournamentMapping[tournamentId];
         uint len = tournament.players.length;
 
@@ -185,7 +186,7 @@ contract ScoreTournament is ProxyStorage {
         }
     }
 
-    function endTournament(uint _tournamentId) public onlyAdmin {
+    function endTournament(uint _tournamentId) public onlyRole(UPDATE_ROLE) {
         calculateRanking(_tournamentId);
         Tournament storage tournament = tournamentMapping[_tournamentId];
         uint _prizeCount = tournament.prizeCount;
@@ -201,7 +202,7 @@ contract ScoreTournament is ProxyStorage {
         emit TournamentEnded(_tournamentId);
     }
 
-    function cancelTournament(uint _tournamentId) public onlyAdmin {
+    function cancelTournament(uint _tournamentId) public onlyRole(UPDATE_ROLE) {
         Tournament storage tournament = tournamentMapping[_tournamentId];
         
         // Get the list of player addresses
