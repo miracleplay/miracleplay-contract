9596c90d379d10669b762d5da0781fc5305a5d20
ArtWide <rkafkko@gmail.com>
2023-12-21 13:24:18 +0900

    edit

--- contract/Miracle-TokenSwap/TokenToToken.sol
+++ contract/Miracle-TokenSwap/TokenToToken.sol
@@ -23,22 +23,15 @@ contract TokenToTokenSwap {
         tokenMUC = IERC20(_tokenMUC);
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the owner");
-        _;
-    }
-
     modifier onlyAdmin() {
-        require(msg.sender == owner, "Not the owner");
+        require(msg.sender == admin, "Not the owner");
         _;
     }
 
-    // 소유자가 B 토큰을 예치하는 함수
     function depositMUC(uint256 amount) public onlyAdmin {
         require(tokenMUC.transferFrom(msg.sender, address(this), amount), "Transfer failed");
     }
 
-    // 사용자가 A 토큰을 보내면 B 토큰을 반환하는 함수
     function swapMZCForMUC(uint256 amount) public {
         require(tokenMZC.transferFrom(msg.sender, address(this), amount), "Transfer of MZC failed");
         require(tokenMUC.transfer(msg.sender, amount), "Transfer of MUC failed");
