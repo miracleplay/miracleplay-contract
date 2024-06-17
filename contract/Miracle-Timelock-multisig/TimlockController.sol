0423762a31e0e319c4d17c011727abc05b9119bd
ArtWide <rkafkko@gmail.com>
2024-06-17 19:29:04 +0900

    update

--- contract/Miracle-Timelock-multisig/TimlockController.sol
+++ contract/Miracle-Timelock-multisig/TimlockController.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@thirdweb-dev/contracts/extension/ContractMetadata.sol";
 
 /**
  * @dev Contract module which acts as a timelocked controller. When set as the
@@ -21,7 +22,8 @@ import "@openzeppelin/contracts/utils/Address.sol";
  * to position this {TimelockController} as the owner of a smart contract, with
  * a multisig or a DAO as the sole proposer.
  */
-contract TimelockController is AccessControl, ERC721Holder, ERC1155Holder {
+contract TimelockController is AccessControl, ERC721Holder, ERC1155Holder, ContractMetadata {
+    address public deployer;
     bytes32 public constant PROPOSER_ROLE = keccak256("PROPOSER_ROLE");
     bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
     bytes32 public constant CANCELLER_ROLE = keccak256("CANCELLER_ROLE");
@@ -133,7 +135,12 @@ contract TimelockController is AccessControl, ERC721Holder, ERC1155Holder {
         }
 
         _minDelay = minDelay;
+        deployer = msg.sender;
         emit MinDelayChange(0, minDelay);
+    }
+
+    function _canSetContractURI() internal view virtual override returns (bool){
+        return msg.sender == deployer;
     }
 
     /**
