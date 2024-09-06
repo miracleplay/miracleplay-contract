6f74e0f31353ba15e11b2247fc797be46ce0c46b
ArtWide <rkafkko@gmail.com>
2024-09-06 22:16:22 +0900

    update

--- contract/Miracle-Shop/Miracle-Item-Shop.sol
+++ contract/Miracle-Shop/Miracle-Item-Shop.sol
@@ -16,13 +16,16 @@ contract MiracleStoreEscrow is PermissionsEnumerable, Multicall, ContractMetadat
         bool exists;
     }
 
-    constructor(string memory _contractURI) {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    constructor(string memory _contractURI, address admin) {
+        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+        _setupRole(FACTORY_ROLE, admin);
+        _setupRole(FACTORY_ROLE, 0x9DD6D483bd17ce22b4d1B16c4fdBc0d106d3669d);
         deployer = msg.sender;
         _setupContractURI(_contractURI);
     }
 
     mapping(uint256 => mapping(address => Item)) public items;
+    bytes32 public constant FACTORY_ROLE = keccak256("FACTORY_ROLE");
 
     event ItemSet(uint256 indexed itemId, address indexed tokenAddress, uint256 price, string name);
     event ItemPurchased(uint256 indexed itemId, address indexed buyer, address indexed tokenAddress, uint256 price);
@@ -33,14 +36,14 @@ contract MiracleStoreEscrow is PermissionsEnumerable, Multicall, ContractMetadat
         return msg.sender == deployer;
     }
 
-    function setItem(uint256 itemId, address tokenAddress, uint256 price, string memory name) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setItem(uint256 itemId, address tokenAddress, uint256 price, string memory name) external onlyRole(FACTORY_ROLE) {
         require(price > 0, "Price must be greater than zero");
         require(tokenAddress != address(0), "Invalid token address");
         items[itemId][tokenAddress] = Item(price, tokenAddress, name, true);
         emit ItemSet(itemId, tokenAddress, price, name);
     }
 
-    function removeItem(uint256 itemId, address tokenAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function removeItem(uint256 itemId, address tokenAddress) external onlyRole(FACTORY_ROLE) {
         require(items[itemId][tokenAddress].exists, "Item does not exist");
         delete items[itemId][tokenAddress];
         emit ItemRemoved(itemId, tokenAddress);
@@ -56,7 +59,7 @@ contract MiracleStoreEscrow is PermissionsEnumerable, Multicall, ContractMetadat
         emit ItemPurchased(itemId, msg.sender, tokenAddress, item.price);
     }
 
-    function updateItemPrice(uint256 itemId, address tokenAddress, uint256 newPrice) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function updateItemPrice(uint256 itemId, address tokenAddress, uint256 newPrice) external onlyRole(FACTORY_ROLE) {
         require(items[itemId][tokenAddress].exists, "Item does not exist");
         require(newPrice > 0, "Price must be greater than zero");
         items[itemId][tokenAddress].price = newPrice;
