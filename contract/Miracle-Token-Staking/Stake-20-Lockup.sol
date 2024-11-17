b42d5035e435fbd25a744009198e4eb565c225dd
ArtWide <rkafkko@gmail.com>
2024-11-17 17:27:46 +0900

    Update Stake-20-Lockup.sol

--- contract/Miracle-Token-Staking/Stake-20-Lockup.sol
+++ contract/Miracle-Token-Staking/Stake-20-Lockup.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+// TimeLockedStakingWithAPR V1.0.0
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
