4eac7e35e9018d602eb2e00a624efac2b6d1748a
ArtWide <rkafkko@gmail.com>
2024-12-15 10:08:00 +0900

    update

---
+++ contract/Tiktrix-tToken/tEntry.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TEntryToken is ERC20, Ownable {
+    uint8 private constant _decimals = 18;
+    uint256 private constant _totalSupply = 1_000_000_000 * 10**18; // 1,000,000,000 tokens
+
+    constructor() ERC20("tEntry", "tEntry") Ownable(msg.sender) {
+        _mint(msg.sender, _totalSupply);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return _decimals;
+    }
+}
