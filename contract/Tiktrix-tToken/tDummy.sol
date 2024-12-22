32efb11a76f0b08b1e501cf130198a43c1d577bf
ArtWide <rkafkko@gmail.com>
2024-12-22 16:08:33 +0900

    update

--- contract/Tiktrix-tToken/tDummy.sol
+++ contract/Tiktrix-tToken/tDummy.sol
@@ -8,7 +8,7 @@ contract TDiscordToken is ERC20, Ownable {
     uint8 private constant _decimals = 18;
     uint256 private constant _totalSupply = 1_000_000_000 * 10**18; // 1,000,000,000 tokens
 
-    constructor() ERC20("tDiscord Token", "tDiscord") Ownable(msg.sender) {
+    constructor() ERC20("tDummy Token", "tDMY") Ownable(msg.sender) {
         _mint(msg.sender, _totalSupply);
     }
 
